83de6b7b75da978ed46269770d959aed361123d9
GrEy <barun.shrestha6@gmail.com>
2020-01-04 20:35:36 +0545

    asd

---
+++ app/src/main/java/com/example/myfurniturestore/BlogActivity.java
@@ -0,0 +1,130 @@
+package com.example.myfurniturestore;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BlogActivity {
+    private RecyclerView recyclerView;
+    List<blog> blogList = new ArrayList<>();
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item2:
+                Intent logoutIntent = new Intent(BlogActivity.this, LoginActivity.class);
+                startActivity(logoutIntent);
+                Toast.makeText(this,"Logged out successfully", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_blogs);
+
+        recyclerView = findViewById(R.id.recyclerview);
+
+        recyclerView.setLayoutManager(new GridLayoutManager(this,1));
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(1, dpToPx(3), true));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(new BlogAdapter(getApplicationContext(),blogList));
+
+        readItems();
+
+    }
+
+
+    private void readItems() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:3000/blog/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        BlogInterface blogApi = retrofit.create(BlogInterface.class);
+
+        Call<List<blog>> listCall = blogApi.getAllblogs();
+
+        listCall.enqueue(new Callback<List<blog>>() {
+            @Override
+            public void onResponse(Call<List<blog>> call, Response<List<blog>> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Code: " + response.code(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                List<blog> blogModelList = response.body();
+
+                recyclerView.setAdapter(new BlogAdapter(getApplicationContext(), blogModelList));
+            }
+
+            @Override
+            public void onFailure(Call<List<blog>> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), "Error " + t.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            int column = position % spanCount; // item column
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
+            }
+        }
+    }
+
+    /**
+     * Converting dp to pixel
+     */
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
+    }
+}
