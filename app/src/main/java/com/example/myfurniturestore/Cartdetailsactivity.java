8277586975332ed2f7f150193373ed28c5b6abe6
GrEy <barun.shrestha6@gmail.com>
2020-01-09 15:36:12 +0545

    cartdetails

---
+++ app/src/main/java/com/example/myfurniturestore/Cartdetailsactivity.java
@@ -0,0 +1,57 @@
+package com.example.myfurniturestore;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Cartdetailsactivity {
+    TextView cartname, cartdescription, cartprice, cartquantity;
+    CircleImageView circleImageView;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item2:
+                Intent logoutIntent = new Intent(Cartdetailsactivity.this, LoginActivity.class);
+                startActivity(logoutIntent);
+                Toast.makeText(this,"Logged out successfully", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_details);
+
+        cartname = findViewById(R.id.cartname);
+        cartdescription = findViewById(R.id.cartdescription);
+        cartprice = findViewById(R.id.cartprice);
+        cartquantity = findViewById(R.id.cartitemquantity);
+        circleImageView = findViewById(R.id.detailImgView);
+
+        Bundle bundle = getIntent().getExtras();
+
+        if (bundle != null){
+            String image = bundle.getString("image");
+            Picasso.with(this).load("http://10.0.2.2:3000/uploads/" + image).into(circleImageView);
+            cartname.setText(bundle.getString("name"));
+            cartprice.setText(bundle.getString("price"));
+            cartquantity.setText(bundle.getString("quantity"));
+            cartdescription.setText(bundle.getString("description"));
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myfurniturestore/ShowCartsActivity.java
@@ -0,0 +1,132 @@
+package com.example.myfurniturestore;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShowCartsActivity {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+
+            case R.id.item2:
+                Intent logoutIntent = new Intent(ShowCartsActivity.this, LoginActivity.class);
+                startActivity(logoutIntent);
+                Toast.makeText(this,"Logged out successfully", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+    private RecyclerView recyclerView;
+    List<carts> cartsList = new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_carts);
+
+        recyclerView = findViewById(R.id.recyclerview);
+
+        recyclerView.setLayoutManager(new GridLayoutManager(this,1));
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(1, dpToPx(3), true));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(new CartAdapter(getApplicationContext(),cartsList));
+
+        readItems();
+
+    }
+
+
+    private void readItems() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:3000/carts/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        CartInterface cartInterface = retrofit.create(CartInterface.class);
+
+        Call<List<carts>> listCall = cartInterface.getAllcarts();
+
+        listCall.enqueue(new Callback<List<carts>>() {
+            @Override
+            public void onResponse(Call<List<carts>> call, Response<List<carts>> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Code: " + response.code(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                List<carts> cartModelList = response.body();
+
+                recyclerView.setAdapter(new CartAdapter(getApplicationContext(), cartModelList));
+            }
+
+            @Override
+            public void onFailure(Call<List<carts>> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), "Error " + t.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            int column = position % spanCount; // item column
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
+            }
+        }
+    }
+
+    /**
+     * Converting dp to pixel
+     */
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
+    }
+}
